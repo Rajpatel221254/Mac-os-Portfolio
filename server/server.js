37491600c24009911a7cc2edc4f901e08d7589bf
Raj Patel <rajpatel221254@gmail.com>
2026-02-16 11:39:43 +0530

    Refactor server port configuration for flexibility

--- server/server.js
+++ server/server.js
@@ -61,6 +61,8 @@ Rules:
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running at http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
